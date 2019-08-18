7aa282acb8bb2fca5879793f8a7707f6e64c0bc3
BingleChen <1358896131@qq.com>
2019-08-18 15:57:37 +0800

    添加导航栏

---
+++ src/main/java/demo/study/community/controller/index.java
@@ -0,0 +1,13 @@
+package main.java.demo.study.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class index {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
